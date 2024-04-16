8df11256286490dcf258e0830ed3d6197d382b67
박철현 <tghy4@naver.com>
2024-04-17 07:22:46 +0900

    에러 메시지 구분

--- src/main/java/hello/itemservice/web/validation/v2/ValidationItemControllerV2.java
+++ src/main/java/hello/itemservice/web/validation/v2/ValidationItemControllerV2.java
@@ -38,9 +38,7 @@ public class ValidationItemControllerV2 {
 
     @GetMapping("/add")
     public String addForm(Model model) {
-        Item item = new Item();
-        item.setId(1L);
-        model.addAttribute("item", item);
+        model.addAttribute("item", new Item());
         return "validation/v2/addForm";
     }
 
@@ -76,7 +74,7 @@ public class ValidationItemControllerV2 {
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v2/items/{itemId}";
     }
-    @PostMapping("/add")
+    /*@PostMapping("/add")*/
     public String addItemV2(@ModelAttribute Item item,BindingResult bindingResult, RedirectAttributes redirectAttributes) {
 
 
@@ -95,6 +93,71 @@ public class ValidationItemControllerV2 {
             int totalPrice = item.getPrice( ) * item.getQuantity();
             if (totalPrice < 10000) {
                 bindingResult.addError(new ObjectError("item",null,null,"ERROR Price * Quantity."));
+            }
+        }
+
+        // if validation failed, show AddForm again
+        if (bindingResult.hasErrors()) {
+            return "validation/v2/addForm";
+        }
+        // success logic
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    /*@PostMapping("/add")*/
+    public String addItemV3(@ModelAttribute Item item,BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+
+        //validation logic, Field Error
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.addError(new FieldError("item","itemName",item.getItemName(),false, new String[]{"required.item.itemName"},null,null));
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 10000000) {
+            bindingResult.addError(new FieldError("item","price",item.getPrice(),false, new String[]{"range.item.price"},new Object[]{1000,1000000},null));
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.addError(new FieldError("item","quantity", item.getQuantity(),false, new String[]{"max.item.quantity"},new Object[]{9999},null));
+        }
+        //Global Error
+        if (item.getPrice() != null && item.getItemName() != null) {
+            int totalPrice = item.getPrice( ) * item.getQuantity();
+            if (totalPrice < 10000) {
+                bindingResult.addError(new ObjectError("item",new String[]{"totalPriceMin"},new Object[]{10000,totalPrice},null));
+            }
+        }
+
+        // if validation failed, show AddForm again
+        if (bindingResult.hasErrors()) {
+            return "validation/v2/addForm";
+        }
+        // success logic
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+    @PostMapping("/add")
+    public String addItemV4(@ModelAttribute Item item,BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+
+        //validation logic, Field Error
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName","required");
+            /*new String[]{"required.item.itemName", "required"};*/
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 10000000) {
+            bindingResult.rejectValue("price","range",new Object[]{1000,100000}, null);
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.rejectValue("quantity","max",new Object[]{9999},null);
+        }
+        //Global Error
+        if (item.getPrice() != null && item.getItemName() != null) {
+            int totalPrice = item.getPrice( ) * item.getQuantity();
+            if (totalPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000,totalPrice},null);
             }
         }
 
