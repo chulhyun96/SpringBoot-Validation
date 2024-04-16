7ffff291cefe4f5b2ff17d6e563ff8c5444290ec
박철현 <tghy4@naver.com>
2024-04-16 16:27:25 +0900

    Validation Version-1

--- src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
+++ src/main/java/hello/itemservice/web/validation/ValidationItemControllerV1.java
@@ -3,13 +3,18 @@ package hello.itemservice.web.validation;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 @RequestMapping("/validation/v1/items")
 @RequiredArgsConstructor
@@ -33,16 +38,50 @@ public class ValidationItemControllerV1 {
 
     @GetMapping("/add")
     public String addForm(Model model) {
-        model.addAttribute("item", new Item());
+        Item item = new Item();
+        item.setId(1L);
+        model.addAttribute("item", item);
         return "validation/v1/addForm";
     }
 
     @PostMapping("/add")
-    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
+    public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes, Model model) {
+        log.info("Item= {}", item);
+        //save errors result
+        Map<String, String> errors = new HashMap<>();
+
+        //validation logic
+        if (!StringUtils.hasText(item.getItemName())) {
+            errors.put("itemName", "Item name is required");
+        }
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 10000000) {
+            errors.put("price", "Price must be between 1000 and 10000000");
+        }
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            errors.put("quantity", "Quantity must be between 0 and 9999");
+        }
+        if (item.getPrice() != null && item.getItemName() != null) {
+            int totalPrice = item.getPrice() * item.getQuantity();
+            if (totalPrice < 10000) {
+                errors.put("global Errors", "Price must be greater than 10000");
+            }
+        }
+
+        // if validation failed, show AddForm again
+        if (hasErrors(errors)) {
+            model.addAttribute("errors", errors);
+            return "validation/v1/addForm";
+        }
+
+        // success logic
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
         return "redirect:/validation/v1/items/{itemId}";
+    }
+
+    private boolean hasErrors(Map<String, String> errors) {
+        return !errors.isEmpty();
     }
 
     @GetMapping("/{itemId}/edit")
